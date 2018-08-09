1afb9590859de6be54ea8a388e6e576d96d6dde0
ljiljana zoric <ljiljana.zoric@norwoodsystems.com>
2018-08-09 23:23:04 +0800

    Revert "Menjam  README file"
    This reverts commit 044da2e8004d473b47d64b44b0b100d571cac9ed.

--- README.md
+++ README.md
@@ -24,4 +24,3 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
-ovo ne znam kako da zaustavim
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "DOBAR DAN SVETE!!!"
+    render html: "¡¡¡hola mundo!"
   end
 end
